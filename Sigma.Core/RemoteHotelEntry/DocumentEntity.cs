e2a985d34be728236267763015456a1675bbf31f
Ivan Berezhnyi <iberezhnoy@gmail.com>
2023-10-08 11:58:05 +0300

    Add DocumentEntity

---
+++ Sigma.Core/RemoteHotelEntry/DocumentEntity.cs
@@ -0,0 +1,94 @@
+﻿namespace Sigma.Core.RemoteHotelEntry
+{
+    public enum DocumentType
+    {
+        Sell,
+        Buy,
+        ReturnFromClient,
+        ReturnToClient
+    }
+
+    public class DocumentsSet : HashSet<DocumentEntity>{}
+
+    public class DocumentEntity
+    {
+        public void Fill(OrganizationEntity organization, ClientEntity client, DateTime date, string? comment, UserEntity user, bool isActive)
+        {
+            Organization = organization;
+            Client = client;
+            Date = date;
+            Comment = comment;
+            User = user;
+            IsActive = isActive;
+        }
+
+        public void SetParentDocument(DocumentEntity? parentDocument)
+        {
+            if (parentDocument != ParentDocument)
+            {
+                ParentDocument = parentDocument;
+            }
+        }
+
+        public void SetChildDocuments(DocumentsSet newChildDocuments)
+        {
+            if (!newChildDocuments.Equals(ChildDocuments))
+            {
+                foreach(DocumentEntity oldChild in ChildDocuments)
+                {
+                    if (!newChildDocuments.Contains(oldChild))
+                    {
+                        if (oldChild.ParentDocument == this)
+                        {
+                            oldChild.SetParentDocument(null);
+                        }
+                    }
+                }
+
+                foreach (DocumentEntity newChild in newChildDocuments)
+                {
+                    if (newChild.ParentDocument != this)
+                    {
+                        newChild.SetParentDocument(this);
+                    }
+                }
+
+                ChildDocuments = newChildDocuments;
+            }
+        }
+
+        public DocumentEntity(string id, OrganizationEntity organization, ClientEntity client, DateTime date, string? comment, UserEntity user, DocumentType documentType, bool isActive)
+        {
+            Id = id;
+            ChildDocuments = new DocumentsSet();
+            DocumentType = documentType;
+
+            Organization = organization;
+            Client = client;
+            Date = date;
+            Comment = comment;
+            User = user;
+            IsActive = isActive;
+
+            Fill(organization, client, date, comment, user, isActive);
+        }
+
+        public string Id { get; set; }
+        public OrganizationEntity Organization { get; set; }
+        public ClientEntity Client { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string? Comment { get; set; }
+
+        public UserEntity User { get; set; }
+
+        public DocumentType DocumentType { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DocumentsSet ChildDocuments { get; set; }
+
+        public DocumentEntity? ParentDocument { get; set; }
+    }
+}
---
+++ Sigma.Core/RemoteHotelEntry/ProductDocumentEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Sigma.Core.RemoteHotelEntry
+{
+    using ProductID = String;
+    public class ProductsSales : Dictionary<ProductID, ProductSaleEntity> { }
+
+    public class ProductDocumentEntity : DocumentEntity
+    {
+        public ProductDocumentEntity(string id, OrganizationEntity organization, ClientEntity client, ProductsSales products, DateTime date, string? comment, UserEntity user, StoreEntity store, DocumentType documentType, bool isActive) 
+            : base(id, organization, client, date, comment, user, documentType, isActive)
+        {
+            Products = products;
+            Store = store;
+        }
+
+        public ProductsSales Products { get; set; }
+
+        public StoreEntity Store { get; set; }
+    }
+}
